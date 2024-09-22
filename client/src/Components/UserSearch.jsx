9921f22da97a48e392099987ad67099a87baadb3
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2024-09-23 00:11:54 +0530

    fix

--- client/src/Components/UserSearch.jsx
+++ client/src/Components/UserSearch.jsx
@@ -1,7 +1,8 @@
 import { Modal } from "antd";
-import React from "react";
+import React, { useState } from "react";
 
 const UserSearch = ({ open, close }) => {
+  const [srchUsers, setsrchUsers] = useState([]);
   return (
     <div>
       <Modal open={open} onCancel={() => close(false)} footer={false}>
@@ -10,16 +11,24 @@ const UserSearch = ({ open, close }) => {
             <input
               type="text"
               placeholder="Search user here..."
-              className=" w-full h-8 px-2 rounded-md border md:h-10"
+              className=" outline-none w-full h-8 px-2 rounded-md border md:h-10"
             />
           </div>
-          <div className=" flex flex-col gap-2 mt-3">
-            <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
-            <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
-            <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
-            <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
-            <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
-          </div>
+          {srchUsers?.length !== 0 ? (
+            <div className=" flex flex-col gap-2 h-[250px] mt-3">
+              <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
+              <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
+              <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
+              <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
+              <div className=" border w-full h-8 md:h-10 bg-slate-200 rounded-md"></div>
+            </div>
+          ) : (
+            <div className=" items-center justify-center flex flex-col gap-2 mt-3 h-[250px]">
+              <p className=" font-semibold text-xl text-gray-200">
+                Search Result show here
+              </p>
+            </div>
+          )}
         </div>
       </Modal>
     </div>
