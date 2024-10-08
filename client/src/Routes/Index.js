e7f502fb3eb6584d5725360269a406094b70c5bb
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2024-10-08 10:49:48 +0530

    frontend deploy02

--- client/src/Routes/Index.js
+++ client/src/Routes/Index.js
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.js";
 import RegisterPage from "../pages/RegisterPage.jsx";
-import CheckEmailPage from "../pages/CheckEmailPage.jsx";
-import CheckPasswordPage from "../pages/CheckPasswordPage.jsx";
 import Home from "../pages/Home.jsx";
 import MessagePage from "../Components/MessagePage.jsx";
 import Login from "../pages/Login.jsx";
@@ -13,14 +11,6 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "register", element: <RegisterPage /> },
-      // {
-      //   path: "email",
-      //   element: <CheckEmailPage />,
-      // },
-      // {
-      //   path: "password",
-      //   element: <CheckPasswordPage />,
-      // },
       {
         path: "login",
         element: <Login />,
